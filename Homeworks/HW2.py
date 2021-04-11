387dd154c978f325bc3feefd9ca231b4e3e05692
Merve Kucukdogru <33333935+Kucukdogru@users.noreply.github.com>
2021-04-11 14:47:50 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,54 @@
-#Explain your work
+#!/usr/bin/env python
+# coding: utf-8
 
-#Question 1
-for x in range(a):
-	print(a)
+# ### User login application
+#  - Get username and password values from the user.
+#  - Check the values in an if statement and tell the user if they were succesful.
+
+# In[19]:
+
+
+kullanici_adi = "merve"
+sifre = "12345"
+print("********Welcome to login application!!!********")
+
+
+# In[33]:
+
+
+username = input("Enter username : ")
+password = input("Enter password : ")
+
+
+# In[35]:
+
+
+if(kullanici_adi == username and sifre == password):
+    print("Congratulations, your information is correct.")
+    print("Hello {}".format(username).upper())
+elif(kullanici_adi != username and sifre == password):
+    print("Invalid username")
+elif(kullanici_adi == username and sifre != password):
+    print("Invalid password")
+elif(kullanici_adi != username and sifre != password):
+    print("Invalid username and password")
+
+
+# ### Extra:
+#  - Try building the same user login application but this time, use a dictionary.
+
+# In[40]:
+
+
+dictionary = {"username":"merve", "password":"12345"}
+kullanici_adi = input("Enter username: ")
+sifre = input("Enter password: ")
+if(dictionary["username"]==kullanici_adi and dictionary["password"]==sifre):
+    print("Congratulations, your information is correct.")
+    print("Hello {}".format(dictionary["username"]).upper())
+elif(dictionary["username"] != kullanici_adi and dictionary["password"] == sifre):
+    print("Invalid username")
+elif(dictionary["username"] == kullanici_adi and dictionary["password"] != sifre):
+    print("Invalid password")
+elif(dictionary["username"] != kullanici_adi and dictionary["password"] != sifre):
+    print("Invalid username and password")
